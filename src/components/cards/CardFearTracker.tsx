022bfc257e6820d0afb6ba603beb78c76f4f4b0c
Jutier <36769570+Jutier@users.noreply.github.com>
2025-09-02 16:58:33 -0300

    v1.0.7 clickable skulls FearTracker

--- src/components/cards/CardFearTracker.tsx
+++ src/components/cards/CardFearTracker.tsx
@@ -1,6 +1,5 @@
 import { BtnClose, BtnDrag } from "../cardButtons/";
 import Card from "../atoms/Card.tsx";
-import { PlusButton, MinusButton } from "../deltaButtons";
 import Skull from "../../assets/skull.png";
 import styled from "styled-components";
 import useCard from "../../contexts/CardContext.ts";
@@ -10,54 +9,58 @@ const SkullRow = styled.span`
 	display: inline-flex;
 	width: 100%;
 	align-items: center;
-	justify-content: center;
+	justify-content: start;
 	gap: 2px;
 `;
 
+const FearCounter = styled.h2`
+	display: inline-flex;
+	color: var(--color-accent-1);
+	font-family: var(--font-title);
+	margin: 0;
+	width: 1.8rem;
+	justify-content: center;
+`;
+
 const SkullImg = styled.img<{ $active: boolean }>`
-	width: 1.715rem;
-	height: 1.715rem;
+	width: 1.75rem;
+	height: 1.75rem;
 	user-select: none;
 	opacity: ${({ $active }) => ($active ? 1 : 0.3)};
-	transition: opacity 0.2s;
+	transition: all 0.2s;
+
+	&:active {
+		transform: scale(0.85);
+	}
 `;
 
 function CardFearTracker() {
 	const card = useCard<{ fearCount?: number}>();
 	const { updateCardExtras } = useBoard();
 	const maxFear = 12;
-	const minFear = 0;
+	// const minFear = 0; // No use for this current implementation
 
-	function handleChange(value: number) {
+	function handleChange(idx: number) {
+		const value = (idx + 1 === card.fearCount) ? idx : idx + 1;
 		updateCardExtras(card, {"fearCount": value})
 	}
 
 	return (
 		<Card>
-			<BtnClose right="0.1rem"/>
-			<BtnDrag right="calc(100% - 1.5rem)"/>
+			<BtnClose right="0.2rem"/>
+			<BtnDrag right="1.6rem"/>
 
 			<SkullRow>
-				<MinusButton
-					state={card.fearCount || 0}
-					setState={handleChange}
-					min={minFear}
-					max={maxFear}
-				/>
-					{[...Array(maxFear)].map((_, idx) => (
-						<SkullImg
-							key={idx}
-							src={Skull}
-							draggable="false"
-							$active={idx < (card.fearCount || 0)}
-						/>
-					))}
-				<PlusButton
-					state={card.fearCount || 0}
-					setState={handleChange}
-					min={minFear}
-					max={maxFear}
-				/>
+				<FearCounter>{card.fearCount}</FearCounter>
+				{[...Array(maxFear)].map((_, idx) => (
+					<SkullImg
+						$active={idx < (card.fearCount || 0)}
+						onClick={() => handleChange(idx)}
+						key={idx}
+						src={Skull}
+						draggable="false"
+					/>
+				))}
 			</SkullRow>
 		</Card>
 	);
